942e61522bbdf5025f5c2c0dd96822ed3c993c1a
vigneshvp <viki3110@gmail.com>
2021-04-25 13:40:41 +0530

    Added method to store address in Database

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/AddressDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/AddressDao.java
@@ -2,17 +2,20 @@ package com.upgrad.FoodOrderingApp.service.dao;
 
 import com.upgrad.FoodOrderingApp.service.entity.Address;
 import org.springframework.stereotype.Repository;
-
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public class AddressDao {
 
     @PersistenceContext
     private EntityManager entityManager;
-
+    
+    @Transactional
     public Address getAddressById(final Integer id) {
         try {
             return entityManager.createNamedQuery("addressById", Address.class)
@@ -21,5 +24,11 @@ public class AddressDao {
         } catch (final NoResultException nre) {
             return null;
         }
+    }
+    
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Address createAddress(Address address) {
+        entityManager.persist(address);
+        return address;
     }
 }
